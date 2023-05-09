61c9574406ac368993f1b136099cb1a626ee9cb6
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-05-09 16:23:45 +0200

    await

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ async function loadData(){
 }
 
 loadData();
+
+
+// ESERCIZIO DATOMI DA FARE DA ANDREA:
+
+
+// Funzione che ritorna true o false se, 
+// in base alla lunghezza dei segmenti dati, essi formano un triangolo:
+
+function isTriangle(a, b, c){
+
+    if(a < b + c && b < a + c && c < a + b){
+        return true;
+    };
+
+}
+
+// Funzione che ritorna true o false se, in base alla lunghezza 
+// di vari segmenti presenti in un array, essi formano una figura chiusa:
+
+function isFigure(array){
+    for (let i = 0; i < array.length; i++) {
+        const segmentsLength = array[i];
+        
+    }
+}; 
+
+// let arrayOfSegmentLength = [10, 5, 10, 5];
+
+// console.log(isFigure(arrayOfSegmentLength));
